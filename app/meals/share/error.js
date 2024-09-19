8f8ab6ccf2cc632e1b930ab3305ea4af7c0989cb
Smileyface101 <beric.vojislav96.pp@gmail.com>
2024-09-19 04:03:50 +0200

    final push
    added dynamic metadata, added some UI changes.

---
+++ app/meals/share/error.js
@@ -0,0 +1,9 @@
+'use client';
+export default function Error({ }) {
+    return (
+        <main className="error">
+            <h1>An Error occured!</h1>
+            <p> One or more fields are missing or you are not using the right format.</p>
+        </main>
+    );
+}
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,13 @@
-
+'use client'
+import { useFormState } from 'react-dom';
 import classes from './page.module.css';
 import ImagePicker from './image-picker';
 import { ShareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meal-submit-form-status';
 export default function ShareMealPage() {
+    const [state, formActionState] = useFormState(ShareMeal, { message: null });
     return (
+
         <>
             <header className={classes.header}>
                 <h1>
@@ -12,7 +16,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={ShareMeal}>
+                <form className={classes.form} action={formActionState}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -41,8 +45,9 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker label='Image Title' name='image' />
+                    {formActionState && <p>{state.message}</p>}
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
